0d7708c27099ff61b0da65a073abf4a963968d34
toSteven <tostevenhoward@gmail.com>
2023-11-21 20:25:21 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+function App() {
+  return (
+    <>
+      <h1 className="display-1 text-center fw-bold mt-5"> Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
